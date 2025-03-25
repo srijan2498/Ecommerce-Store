77f48cd62025cfdc6b0981144af34a55bda53b02
Srijan Tripathi <47470062+srijan2498@users.noreply.github.com>
2025-03-25 19:52:20 +0530

    Update index.js

--- ecommerce-store/backend/index.js
+++ ecommerce-store/backend/index.js
@@ -33,9 +33,9 @@ app.use("/api/admin", adminRouter);
 
 const PORT = process.env.PORT || 8000;
 
-app.use(express.static(path.join(__dirname, "./frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`.bgCyan));
